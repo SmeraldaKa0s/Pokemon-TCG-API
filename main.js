0e0b7e09a689bb0c15747473ff1c3d7bd6b02372
unknown <sofiabernabeicejas@gmail.com>
2022-01-11 21:06:57 -0300

    estilos tarjetas

--- main.js
+++ main.js
@@ -1,75 +1,121 @@
-const contenedorTarjetas = document.querySelector(".tarjetas")
-console.log(contenedorTarjetas)
+const contenedorTarjetas = document.querySelector(".tarjetas");
+const contenedorSets = document.querySelector(".cotenedor-sets");
+
 // Paginado
-const firstPage = document.getElementById("first-page")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const lastPage = document.getElementById("last-page")
+const firstPage = document.getElementById("first-page");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const lastPage = document.getElementById("last-page");
 
-let paginaActual = 1
-let ultimaPagina = 0
+let paginaActual = 1;
+let ultimaPagina = 0;
 
-const urlPokemon = async () => {    
-    const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?pageSize=10&page=${paginaActual}`)
-    const data = await respuesta.json()
-    console.log(data)
-    console.log(data.data)
-    contenedorTarjetas.innerHTML = aHTML(data)     
-}    
+const urlPokemon = async () => {
+	const respuesta = await fetch(
+		`https://api.pokemontcg.io/v2/cards?pageSize=10&page=${paginaActual}`
+	);
+	const data = await respuesta.json();
+	console.log(data);
+	console.log(data.data);
+	contenedorTarjetas.innerHTML = aHTML(data);
+};
 
-urlPokemon()
+urlPokemon();
 
 const aHTML = (data) => {
-    const arrayReduc = data.data.reduce((acc, elemento) => {
-        return acc + `
+	const arrayReduc = data.data.reduce((acc, elemento) => {
+		return (
+			acc +
+			`
         <div class="item" id="${elemento.id}">
         <img class="card-img" src="${elemento.images.large}" alt="${elemento.name}">
         </div>`
-    }, "")
-    
-    return arrayReduc
-} 
+		);
+	}, "");
+
+	return arrayReduc;
+};
+
+//Sets
+
+const setsPokemon = async () => {
+	const respuesta = await fetch(`https://api.pokemontcg.io/v2/sets`);
+	const data = await respuesta.json();
+	console.log(data);
+	console.log(data.data);
+
+	contenedorSets.innerHTML = setsHTML(data);
+};
+
+setsPokemon();
+
+const setsHTML = (data) => {
+	const arraySets = data.data.reduce((acc, elemento) => {
+		return (
+			acc +
+			`
+			<div class="tarjetas-sets">
+               <div class="cotenedor-imagen-sets">
+                  <img src="${elemento.images.logo}">
+               </div>
+				<div>
+				     <p> ${elemento.name} </p>
+				     <p> ${elemento.id} </p>
+						 <p> ${elemento.releaseDate} </p>
+				     <p> ${elemento.series} </p>
+                </div>
+			  	<div class="contenedor-logo-sets">
+                   <img src="${elemento.images.symbol}">
+                </div>
+			
+			</div>
+			`
+		);
+	}, "");
+
+	return arraySets;
+};
 
 // Paginado
 
 firstPage.onclick = () => {
-    paginaActual = 1
-    firstPage.disabled = true
-    prev.disabled = true
-    next.disabled = false
-    lastPage.disabled = false
-    urlPokemon()
-}
+	paginaActual = 1;
+	firstPage.disabled = true;
+	prev.disabled = true;
+	next.disabled = false;
+	lastPage.disabled = false;
+	urlPokemon();
+};
 
 next.onclick = () => {
-    paginaActual++ 
-    firstPage.disabled = false
-    prev.disabled = false
-     if (paginaActual === 1441) {
-      next.disabled = true
-      lastPage.disabled = true
-    } 
-    urlPokemon()
-}
+	paginaActual++;
+	firstPage.disabled = false;
+	prev.disabled = false;
+	if (paginaActual === 1441) {
+		next.disabled = true;
+		lastPage.disabled = true;
+	}
+	urlPokemon();
+};
 
 prev.onclick = () => {
-    paginaActual--
-    next.disabled = false
-    lastPage.disabled = false
-    if (paginaActual === 1) {
-        prev.disabled = true
-        firstPage.disabled = true
-    }
-    urlPokemon()
-}
+	paginaActual--;
+	next.disabled = false;
+	lastPage.disabled = false;
+	if (paginaActual === 1) {
+		prev.disabled = true;
+		firstPage.disabled = true;
+	}
+	urlPokemon();
+};
 
 lastPage.onclick = () => {
-    paginaActual = 1441
-    prev.disabled = false
-    firstPage.disabled = false
-    if (paginaActual === 1441) {
-        next.disabled = true
-        lastPage.disabled = true
-    }
-    urlPokemon()
-}
+	paginaActual = 1441;
+	prev.disabled = false;
+	firstPage.disabled = false;
+	if (paginaActual === 1441) {
+		next.disabled = true;
+		lastPage.disabled = true;
+	}
+	urlPokemon();
+};
