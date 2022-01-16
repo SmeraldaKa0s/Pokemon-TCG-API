01a2ba257d364c1f90f2bc097ce697b3f371714a
Elizabeth Carolina <celerner4@gmail.com>
2022-01-16 16:03:00 -0300

    agrego input de búsqueda en el formulario, corrijo posicionamiento de los botones

--- main.js
+++ main.js
@@ -11,6 +11,40 @@ const lastPage = document.getElementById("last-page")
 let paginaActual = 1
 let ultimaPagina = 0
 
+const tablasHTML = (data) => {
+    const arrayAHtml = data.data.reduce((acc, elemento) => {
+        return acc + `
+        <tbody>
+            <tr>
+                <td>${elemento.name}</td>
+                <td>${elemento.nationalPokedexNumbers}</td>
+                <td>${elemento.set.name}</td>
+                <td>${elemento.rarity}</td>
+                <td>${elemento.types[0]}</td>
+                <td>${elemento.subtypes[0]}</td>
+                <td>${elemento.resistances && elemento.resistances.length && elemento.resistances[0].type ? elemento.resistances[0].type : "None"}</td>
+                <td>${elemento.weaknesses && elemento.weaknesses.length && elemento.weaknesses[0].type ? elemento.weaknesses[0].type : "None"}</td>            
+            </tr>
+        </tbody>
+        `
+    }, `<thead>
+           <tr>
+                <th>Name</th>
+                <th>N.Pokedex</th>
+                <th>Set</th>
+                <th>Rarity</th>
+                <th>Types</th>
+                <th>Subtypes</th>
+                <th>Resistances</th>
+                <th>Weaknesses</th>
+            </tr>
+        </thead> `
+    )
+
+    return arrayAHtml
+}
+
+
 const urlPokemon = async () => {    
     const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?pageSize=12&page=${paginaActual}`)
     const data = await respuesta.json()
@@ -76,40 +110,6 @@ prev.onclick = () => {
     }
     urlPokemon()
 }
-
-const tablasHTML = (data) => {
-    const arrayAHtml = data.data.reduce((acc, elemento) => {
-        return acc + `
-        <tbody>
-            <tr>
-                <td>${elemento.name}</td>
-                <td>${elemento.nationalPokedexNumbers}</td>
-                <td>${elemento.set.name}</td>
-                <td>${elemento.rarity}</td>
-                <td>${elemento.types[0]}</td>
-                <td>${elemento.subtypes[0]}</td>
-                <td>${elemento.resistances && elemento.resistances.length && elemento.resistances[0].type ? elemento.resistances[0].type : "None"}</td>
-                <td>${elemento.weaknesses && elemento.weaknesses.length && elemento.weaknesses[0].type ? elemento.weaknesses[0].type : "None"}</td>            
-            </tr>
-        </tbody>
-        `
-    }, `<thead>
-           <tr>
-                <th>Name</th>
-                <th>N.Pokedex</th>
-                <th>Set</th>
-                <th>Rarity</th>
-                <th>Types</th>
-                <th>Subtypes</th>
-                <th>Resistances</th>
-                <th>Weaknesses</th>
-            </tr>
-        </thead> `
-    )
-
-    return arrayAHtml
-}
-
 
 lastPage.onclick = () => {
     paginaActual = 1441
