31f10e3f0b0a88bb5456f1396776aad53d4ba91e
SmeraldaKa0s <anabella.lerner@gmail.com>
2022-01-18 17:03:08 -0300

    borro codigo repetido y arreglo nombre variable

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const next = document.getElementById("next")
 const lastPage = document.getElementById("last-page")
 
 //Buscador
-const searchForm = document.getElementById("serch-form")
+const searchForm = document.getElementById("search-form")
 const searchInput = document.getElementById("search-input")
 
 const paginadoBusqueda = document.getElementById("paginado-busqueda")
@@ -25,24 +25,11 @@ let ultimaPagina = 0
 const urlPokemon = async () => {    
     const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?pageSize=12&page=${paginaActual}`)
     const data = await respuesta.json()
-    console.log(data)
-    console.log(data.data)
     contenedorTarjetas.innerHTML = aHTML(data) 
-    console.log(data.data[0].resistances[0].type) 
-    console.log(tablasHTML(data))
     tablaInfoPokemon.innerHTML = tablasHTML(data)
-    console.log(data.data[0])
-
-   
-    
-    contenedorTarjetas.innerHTML = aHTML(data)     
-}    
+}
 
 urlPokemon()
-
-const fetchBusquedaPokemon = async () => {
-    
-}
 
 const aHTML = (data) => {
     const arrayAHtml = data.data.reduce((acc, elemento) => {
@@ -73,7 +60,7 @@ next.onclick = () => {
      if (paginaActual === 1441) {
       next.disabled = true
       lastPage.disabled = true
-    } 
+    }
     urlPokemon()
 }
 
@@ -121,15 +108,6 @@ const tablasHTML = (data) => {
     return arrayAHtml
 }
 
-
-const urlPokemon = async () => {    
-    const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?pageSize=10&page=${paginaActual}`)
-   
-    const data = await respuesta.json()   
-    contenedorTarjetas.innerHTML = aHTML(data)     
-  
-}    
-
 urlPokemon()
 
 const fetchBusquedaPokemon = async () => {
@@ -140,17 +118,6 @@ const fetchBusquedaPokemon = async () => {
 }
 
 fetchBusquedaPokemon()
-
-const aHTML = (data) => {
-    const arrayAHtml = data.data.reduce((acc, elemento) => {
-        return acc + `
-        <div class="item" id="${elemento.id}">
-        <img class="card-img" src="${elemento.images.large}" alt="${elemento.name}">
-        </div>`
-    }, "")
-    
-    return arrayAHtml
-} 
 
 // Paginado
 
