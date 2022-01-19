fb31e047f4a465981447ae8b51e93a9c7424ecca
unknown <sofiabernabeicejas@gmail.com>
2022-01-19 00:17:08 -0300

    paginado comienzo

--- main.js
+++ main.js
@@ -23,6 +23,54 @@ const lastPage = document.getElementById("last-page");
 let paginaActual = 1;
 let ultimaPagina = 0;
 
+//paginado sets
+const firstPageSets = document.getElementById("first-page-set");
+const prevSets = document.getElementById("prev-set");
+const nextSets = document.getElementById("next-set");
+const lastPageSets = document.getElementById("last-page-set");
+
+// firstPageSets.onclick = () => {
+// 	paginaActual = 1;
+// 	firstPageSets.disabled = true;
+// 	prevSets.disabled = true;
+// 	nextSets.disabled = false;
+// 	lastPageSets.disabled = false;
+// 	setsPokemon();
+// };
+
+// nextSets.onclick = () => {
+// 	paginaActual++;
+// 	firstPageSets.disabled = false;
+// 	prev.disabled = false;
+// 	if (paginaActual === 1441) {
+// 		nextSets.disabled = true;
+// 		lastPageSets.disabled = true;
+// 	}
+// 	setsPokemon();
+// };
+
+// prevSets.onclick = () => {
+// 	paginaActual--;
+// 	nextSets.disabled = false;
+// 	lastPageSets.disabled = false;
+// 	if (paginaActual === 1) {
+// 		prevSets.disabled = true;
+// 		firstPageSets.disabled = true;
+// 	}
+// 	setsPokemon();
+// };
+
+// lastPageSets.onclick = () => {
+// 	paginaActual = 1441;
+// 	prevSets.disabled = false;
+// 	firstPageSets.disabled = false;
+// 	if (paginaActual === 1441) {
+// 		nextSets.disabled = true;
+// 		lastPageSets.disabled = true;
+// 	}
+// 	setsPokemon();
+// };
+
 //Sets
 
 const setsPokemon = async () => {
@@ -30,9 +78,6 @@ const setsPokemon = async () => {
 		`https://api.pokemontcg.io/v2/sets?pageSize=20&page=${paginaActual}`
 	);
 	const data = await respuesta.json();
-	console.log(data);
-	console.log(data.data);
-
 	contenedorSets.innerHTML = setsHTML(data);
 };
 
@@ -54,10 +99,10 @@ const setsHTML = (data) => {
               </div>
 							<div class="contenedor-texto-sets">
 				         <p> Name: ${elemento.name} </p>
-				         <p> Id:${elemento.id} </p>
 						 <p> Date: ${elemento.releaseDate} </p>
                          <p> Total number of cards: ${elemento.total} </p>
 				         <p> Serie: ${elemento.series} </p>
+                         <p> Legalities: ${elemento.legalities.unlimited} </p>
                          <p> Code: ${elemento.ptcgoCode} </p>
                </div>
            </div>
