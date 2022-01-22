d5994e057358f3a52c7411252510414b68fd3f4c
Elizabeth Carolina <celerner4@gmail.com>
2022-01-22 17:25:42 -0300

    agrego js a cada html

--- main.js
+++ main.js
@@ -2,17 +2,7 @@
 //const contenedorTarjetas = document.querySelector(".tarjetas");
 const contenedorSets = document.querySelector(".cotenedor-sets")
 const contenedorTarjetas = document.querySelector("#tarjetas")
-const contenedorTabla = document.querySelector(".tablas")
-const contenedorCartas = document.querySelector("#contenedor-cartas")
-const tablaInfoPokemon = document.querySelector("#tabla-resultados")
-const inputBusquedaCartaIndividual = document.querySelector("#input-busqueda-carta-individual")
-const selectorVerComo = document.querySelector("#selector-ver-como")
-const selectorOrdenarPorNombreYNumero = document.querySelector("#selector-ordenar-nombre-numero")
-const selectorOrdenarPorAscDesc = document.querySelector("#selector-ordenar-asc-desc")
-const formularioCartaIndividual = document.querySelector(".formularios")
-const botonSiguienteTablasCard = document.querySelector("#button-siguiente")
-const botonAnteriorTablasCard = document.querySelector("#button-anterior")
-const botonPrimeraPaginaTablasCard = document.querySelector("#boton-primera-pagina")
+
 // Paginado
 const firstPage = document.getElementById("first-page");
 const prev = document.getElementById("prev");
@@ -37,118 +27,6 @@ const aHTML = (data) => {
 
 
 // CARTA-INDIVIDUAL.HTML 
-
-const fetchBusquedaTablasEImagenes = async () => {
-    const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?q=name:${inputBusquedaCartaIndividual.value}&pageSize=20&page=${paginaActual}`)
-    const data = await respuesta.json()
-    contenedorCartas.innerHTML = aHTML(data)
-    verComoEnHtml(data)
-   
-}
-
-const fetchTablasEImagenes = async () => {
-    const respuesta = await fetch(`https://api.pokemontcg.io/v2/cards?pageSize=20&page=${paginaActual}`)
-    const data = await respuesta.json()
-    contenedorCartas.innerHTML = aHTML(data)
-    verComoEnHtml(data)
-}
-
-fetchTablasEImagenes()
-
-const tablasHTML = (data) => {
-    const arrayAHtml = data.data.reduce((acc, elemento) => {
-        return acc + `
-        <tbody>
-            <tr>
-                <td>
-                ${elemento.name}
-                </td>
-                <td>
-                ${elemento.nationalPokedexNumbers}
-                </td>
-                <td>
-                ${elemento.set.name}
-                </td>
-                <td>
-                ${elemento.rarity ? elemento.rarity : "None"}
-                </td>
-                <td>
-                ${elemento.types[0]}
-                </td>
-                <td>
-                ${elemento.subtypes[0]}
-                </td>
-                <td>
-                ${elemento.resistances && elemento.resistances.length && elemento.resistances[0].type ? elemento.resistances[0].type : "None"}
-                </td>
-                <td>
-                ${elemento.weaknesses && elemento.weaknesses.length && elemento.weaknesses[0].type ? elemento.weaknesses[0].type : "None"}
-                </td>            
-            </tr>
-        </tbody>
-        `
-    }, `
-    <thead>
-           <tr>
-                <th>
-                Name
-                </th>
-                <th>
-                National Pokedex
-                </th>
-                <th>
-                Set
-                </th>
-                <th>
-                Rarity
-                </th>
-                <th>
-                Types
-                </th>
-                <th>
-                Subtypes
-                </th>
-                <th>
-                Resistances
-                </th>
-                <th>
-                Weaknesses
-                </th>
-            </tr>
-    </thead> `
-    )
-
-    return arrayAHtml
-}
-
-const verComoEnHtml = (d) => {
-
-    const data = d
-
-    selectorVerComo.onchange = () => {    
-        if (selectorVerComo.value === "images") {
-            contenedorCartas.innerHTML = aHTML(data)
-            contenedorTabla.style.display = "none"
-            contenedorCartas.style.display = "flex"
-        }
-        else {
-            tablaInfoPokemon.innerHTML = tablasHTML(data)            
-            contenedorTabla.style.display = "flex"
-            contenedorCartas.style.display = "none"
-        }
-    }
-}
-
-formularioCartaIndividual.onsubmit = (e) => {
-    e.preventDefault()
-    fetchBusquedaTablasEImagenes()
-    //contenedorCartas.innerHTML = aHTML(data)
-}
-
-botonPrimeraPaginaTablasCard.onclick = () => paginaActual = 1 && fetchBusquedaTablasEImagenes()
-botonSiguienteTablasCard.onclick = () => (paginaActual++ && fetchBusquedaTablasEImagenes())
-botonAnteriorTablasCard.onclick = () => paginaActual !== 1 && (paginaActual-- && fetchBusquedaTablasEImagenes())  
-
 
 
 
